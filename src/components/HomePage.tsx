a6df75d410b22a1965c6e1df8d4c1143d4a2613e
Tukuna Patro <tukunapatro@Swatis-MacBook-Pro.local>
2022-04-22 23:40:14 +0530

    update social links

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,6 @@
-import Navbar from "./Navbar";
 import MainSection from "./MainSection";
 import Features from "./Features";
 import Courses from "./Courses";
-import Footer from "./Footer";
 
 const HomePage = () => {
   return (
