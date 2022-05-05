b3e9fba715ff51d800d18f61d151d5d58698def4
Tukuna Patro <tukunapatro@Swatis-MacBook-Pro.local>
2022-05-06 02:37:49 +0530

    update policy sections

---
+++ src/components/ReturnsPolicy.tsx
@@ -0,0 +1,77 @@
+const ReturnPolicy = () => {
+  return (
+    <div className="text-[black]">
+      <div>
+        <b>
+          Thank you for trusting and starting your learning journey with us.
+          <br />
+        </b>{" "}
+        <b>Genlent</b> ensures that you have a rewarding experience for any
+        instructor-led or self-paced courses you purchase. As with any online
+        purchase experience, there are terms and conditions that govern the
+        Refund Policy. When you buy a course on <b>Genlent</b>, you agree to our
+        Privacy Policy, Terms of service and Refund Policy.
+      </div>
+      <br />
+      <br />
+      <h1 className="text-xl">
+        <b>Cancellation & Refunds: </b>
+      </h1>
+      <br />
+      <br />
+      <div>
+        <b>Online Training For Self Placed Learning:-</b>
+        <br />
+        Raise refund request within 10 days of purchase of a course. Any refund
+        request beyond 10 days of purchasing the course will not be accepted and
+        no refund will be provided.
+      </div>
+      <br />
+      <div>
+        <b>For Instructor Led Training:- </b>
+        <br />
+        Raise refund request within 10 days of purchase of course. Any refund
+        request beyond 10 days of purchasing the course will not be accepted and
+        no refund will be provided.
+      </div>
+      <br />
+
+      <div>
+        <b>For Offline Courses:-</b>
+        <br />
+        <b>Genlent</b>, reserves the right to postpone/cancel an event, or
+        change the location of an event because of insufficient enrollments,
+        instructor illness or for natural and unavoidable catastrophes (like
+        floods, earthquakes, political instability, etc) In case <b>Genlent</b>{" "}
+        cancels an event, 100% of course fees will be refunded to the
+        participant. However travel, logistics or any personal expense incurred
+        by participants will not be refunded. In case Genlent cancels/postpones
+        an event, the participants who are ahead of 15 days of purchase, of
+        course, will be rescheduled to any upcoming batch without any extra
+        charges. If a cancellation is done by a participant within 7 business
+        days (or less) of the event, no refunds will be made. No refunds or
+        credits will be available for participants who fail to attend the
+        course.
+        <br />
+        <br />
+        <b>Raising Refund Request:-</b>
+        <div>
+          Refund request can be initiated by reaching out to our support team
+          through our{" "}
+          <a href="/about" className="text-[blue]">
+            contact
+          </a>{" "}
+          section on the website. Refund of the duplicate payment made by the
+          participant will be processed via the same source (original method of
+          payment) in 7 working days post intimation by the participant.{" "}
+          <b>Genlent</b> reserves the right to revise the terms & conditions of
+          this policy without any prior notice. <br />
+          Note: All refunds will be processed within 10 working days after the
+          refund request is approved by <b>Genlent</b>.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReturnPolicy;
