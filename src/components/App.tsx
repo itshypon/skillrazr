a79f1ae1e46de34049d08f5cabc01b66f4c5d61d
Tukuna Patro <tukuna.patro@gmail.com>
2022-06-01 19:55:30 +0530

    add world day message banner

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,6 +11,8 @@ import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import { useNavigate } from "react-router-dom";
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import days from '../days';
 
 interface State extends SnackbarOrigin {
   open: boolean;
@@ -59,7 +61,61 @@ function Snack() {
   );
 }
 
+function DaySnack(props: any) {
+  const { messages, url, handleClick } = props;
+  let navigate = useNavigate();
+
+  const [state, setState] = React.useState<State>({
+    open: true,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal, open } = state;
+
+  const handleClose = () => {
+    handleClick(true);
+    setState({ ...state, open: false });
+  };
+
+  return (
+    <div>
+      <Snackbar className='!top-[110px] sm:!top-[70px] cursor-pointer'
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        onClose={handleClose}
+        message={<div><NewspaperIcon /><span className='ml-2 font-bold inline-block'>Today is :- {messages[0]}</span> <div>{messages[1]}</div></div>}
+        key={vertical + horizontal}
+        onClick={() => {
+          handleClick(true);
+          if (url) {
+            navigate(`../${url}`, { replace: false });
+            handleClose();
+          }
+        }}
+        action={<IconButton
+          aria-label="close"
+          color="inherit"
+          sx={{ p: 0.5 }}
+          onClick={(e) => {
+            handleClose();
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+        >
+          <CloseIcon />
+        </IconButton>}
+      />
+    </div >
+  );
+}
+
 function App(props: any) {
+  const date = new Date();
+  const dateMonth = `${date.getDate()}/${date.getMonth() + 1}`;
+  const [daySnackSeen, setDaySnackSeen] = React.useState(false);
+
+  console.log('message today', days, dateMonth, days[dateMonth]);
+
   return (
     <ParallaxProvider>
       <BrowserRouter>
@@ -85,7 +141,7 @@ function App(props: any) {
             />
           </Route>
         </Routes>
-        <Snack />
+        {days[dateMonth] && !daySnackSeen ? <DaySnack messages={days[dateMonth]} handleClick={setDaySnackSeen} /> : <Snack />}
       </BrowserRouter>
     </ParallaxProvider>
   );
