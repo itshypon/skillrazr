d8e9d25617b7f6ed16b0d81be36e6fe880ad5b8a
tukuna30 <tukuna.patro@gmail.com>
2022-12-18 21:16:26 +0530

    final touch planty game

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -2,12 +2,11 @@ import { NavLink } from "react-router-dom";
 
 export default function Games(props: any) {
   const availalbeGames = [
-    // {
-    //   id: "planty",
-    //   title: "Plant Trees",
-    //   icon: LooksOneIcon,
-    //   description: "Plant trees riding a horse",
-    // },
+    {
+      id: "planty",
+      title: "Plant Trees",
+      description: "Plant trees riding a horse",
+    },
     {
       id: "scratchy",
       title: "Scratchy",
