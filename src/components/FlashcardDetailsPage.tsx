de366494d3ac819c3629752a2ebf7354c2bf0b34
r171146@rguktrkv.ac.in <RanjithKumarSeekolu>
2023-04-15 18:14:30 +0530

    first commit - add_flashcards_on_various_topics

--- src/components/FlashcardDetailsPage.tsx
+++ src/components/FlashcardDetailsPage.tsx
@@ -1,5 +1,14 @@
 import { useParams } from "react-router-dom";
 import { flashcards } from "../data/flashcards";
+import { useState, useEffect } from "react";
+import "./FlashcardDetailsPage.css";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+  integrity="sha512-q1vfLJkiTtYrXtTjDLZPvMXefPNTwFYzVhKj67zp0z+VkC1Qtt0HkX8so0+uvZc7z3e3JFdH/y+KEDGGksyIjA=="
+  crossOrigin="anonymous"
+  referrerPolicy="no-referrer"
+/>;
 
 function FlashcardDetailsPage() {
   const { id: category } = useParams();
@@ -9,18 +18,114 @@ function FlashcardDetailsPage() {
     (flashcard) => flashcard.category === category
   );
 
+  console.log(matchingFlashcards);
+
   // get all the flashcards belongs to the id. e.g javascript
 
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleNextClick = () => {
+    if (currentQuestionIndex < matchingFlashcards.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setIsFlipped(false);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+      setIsFlipped(false);
+    }
+  };
+
+  useEffect(() => {
+    function handleArrowKeys(event: { keyCode: number }) {
+      if (event.keyCode === 37) {
+        // left arrow key
+        handlePrevClick();
+      } else if (event.keyCode === 39) {
+        // right arrow key
+        handleNextClick();
+      }
+    }
+
+    window.addEventListener("keydown", handleArrowKeys);
+    return () => {
+      window.removeEventListener("keydown", handleArrowKeys);
+    };
+  }, [currentQuestionIndex]);
+
   return (
-    <div>
-      {matchingFlashcards.map((flashcard) => {
-        return (
-          <>
-            <div>{flashcard.question}</div>;<div>{flashcard.answer}</div>
-          </>
-        );
-      })}
-    </div>
+    <>
+      {matchingFlashcards.length > 0 && (
+        <>
+          <div className="heading">
+            <h1 className="category">
+              {matchingFlashcards[currentQuestionIndex].category}
+            </h1>
+            <h1 className="description">
+              {`- ${matchingFlashcards[currentQuestionIndex].description}`}
+            </h1>
+          </div>
+          <div className="flashcard-container">
+            <div className="question-number">
+              <p>{`${currentQuestionIndex + 1} / ${
+                matchingFlashcards.length
+              }`}</p>
+            </div>
+            <div className={`card ${isFlipped ? "flipped" : ""}`}>
+              <div className="front">
+                <h2 className="question">{`Q${currentQuestionIndex + 1}) ${
+                  matchingFlashcards[currentQuestionIndex].question
+                }`}</h2>
+                <div className="button-container">
+                  {currentQuestionIndex === 0 ? (
+                    <p></p>
+                  ) : (
+                    <button className="prev-button" onClick={handlePrevClick}>
+                      <i className="fa fa-chevron-left"></i>
+                    </button>
+                  )}
+
+                  {currentQuestionIndex !== matchingFlashcards.length - 1 && (
+                    <button className="next-button" onClick={handleNextClick}>
+                      <i className="fa fa-chevron-right"></i>
+                    </button>
+                  )}
+                </div>
+                <button className="flip-button" onClick={handleClick}>
+                  Flip
+                </button>
+              </div>
+              <div className="back">
+                <h2>Answer:</h2>
+                <h2 className="answer">
+                  {matchingFlashcards[currentQuestionIndex].answer}
+                </h2>
+                <button className="flip-button" onClick={handleClick}>
+                  Flip
+                </button>
+                {!isFlipped && (
+                  <>
+                    <button className="prev-button" onClick={handlePrevClick}>
+                      Prev
+                    </button>
+                    <button className="next-button" onClick={handleNextClick}>
+                      Next
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
--- src/data/flashcards.ts
+++ src/data/flashcards.ts
@@ -11,6 +11,12 @@ export const flashcardCategories = [
     title: "CSS",
     description: "Flashcards to memorise CSS concepts easily",
   },
+  {
+    id: 1,
+    category: "html",
+    title: "HTML",
+    description: "Flashcards to memorise HTML concepts easily",
+  },
 ];
 
 export const flashcards = [
@@ -21,7 +27,6 @@ export const flashcards = [
     category: "javascript",
     description: "JS closure",
   },
-
   {
     id: 2,
     question: "What is hoisting in JS?",
@@ -30,15 +35,119 @@ export const flashcards = [
     description: "JS hoisting",
   },
   {
+    id: 3,
+    question:
+      "What is the difference between let, var, and const in JavaScript?",
+    answer: `let and const were introduced in ES6 and provide block-level scoping, while var has function-level scoping. const is used to declare a constant value that cannot be reassigned, while let can be reassigned.`,
+    category: "javascript",
+    description: "let vs var vs const",
+  },
+  {
+    id: 4,
+    question: "What is an event in JavaScript?",
+    answer: `An event is an action or occurrence that happens on a web page, such as a click, scroll, or key press, that can trigger JavaScript code to perform a specific function.`,
+    category: "javascript",
+    description: "events",
+  },
+  {
+    id: 5,
+    question: "What is a callback function in JavaScript?",
+    answer: `A callback function is a function that is passed as an argument to another function and is executed after some specific event or action takes place.`,
+    category: "javascript",
+    description: "JS Callback function",
+  },
+  {
+    id: 6,
+    question: "What is the DOM in JavaScript?",
+    answer: `The Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the page so that JavaScript can change and manipulate the content.`,
+    category: "javascript",
+    description: "JS DOM",
+  },
+
+  {
     id: 1,
     question: "What is positioning in CSS?",
-    answer: `In CSS, there are four types of positioning: static, relative, absolute, and fixed. Here's a summary of each type:
-    Static: This is the default positioning for all elements. The element is positioned in the normal flow of the document, according to its position in the HTML.
-    Relative: This type of positioning allows an element to be positioned relative to its normal position in the HTML. The element is still positioned in the normal flow of the document, but can be moved using the top, bottom, left, and right properties.
-    Absolute: This type of positioning allows an element to be positioned relative to its nearest positioned ancestor (an ancestor element with a position value other than static). If there is no positioned ancestor, the element is positioned relative to the body element. The element is removed from the normal flow of the document, so other elements can flow around it.
-    Fixed: This type of positioning is similar to absolute positioning, but the element is positioned relative to the viewport (the browser window), rather than an ancestor element. The element is also removed from the normal flow of the document.
-    In addition to these four types of positioning, there are also z-index and float properties that can affect the positioning of elements. The z-index property specifies the stack order of an element, and the float property allows an element to be floated to the left or right of its container.`,
+    answer: `Positioning in CSS allows you to control the layout and positioning of HTML elements on a web page. It includes various properties such as position, top, bottom, left, and right, which can be used to position elements relative to their containing element or to the browser window.`,
     category: "css",
     description: "CSS positioning",
+  },
+  {
+    id: 2,
+    question: "What is the syntax for adding CSS to an HTML document?",
+    answer: `CSS can be added to an HTML document using the <style> tag or by linking to an external CSS file using the <link> tag. in CSS allows you to control the layout and positioning of HTML elements on a web page. It includes various properties such as position, top, bottom, left, and right, which can be used to position elements relative to their containing element or to the browser window.`,
+    category: "css",
+    description: "syntax of css",
+  },
+  {
+    id: 3,
+    question: "What is the difference between padding and margin in CSS?",
+    answer: `Padding is the space within an element, while margin is the space outside of an element`,
+    category: "css",
+    description: "CSS padding & margin",
+  },
+  {
+    id: 4,
+    question: "What is the box model in CSS?",
+    answer: `The box model is a design concept in CSS that treats every element on a webpage as a rectangular box.`,
+    category: "css",
+    description: "box model",
+  },
+  {
+    id: 5,
+    question: "What is the purpose of the z-index property in CSS?",
+    answer: `The z-index property is used to control the stacking order of elements on a webpage.s a design concept in CSS that treats every element on a webpage.`,
+    category: "css",
+    description: "z-index",
+  },
+  {
+    id: 6,
+    question:
+      "What is the difference between inline and block elements in CSS?",
+    answer: `Inline elements are displayed on the same line as their surrounding content, while block elements start on a new line.`,
+    category: "css",
+    description: "inline vs block",
+  },
+  {
+    id: 7,
+    question: "What is the purpose of the float property in CSS?",
+    answer: `The float property is used to position elements to the left or right of their container.`,
+    category: "css",
+    description: "float property",
+  },
+  {
+    id: 8,
+    question:
+      "What is the difference between absolute and relative positioning in CSS?",
+    answer: ` Absolute positioning places an element relative to the nearest positioned ancestor, while relative positioning places an element relative to its normal position on the page.`,
+    category: "css",
+    description: "absolute vs relative positioning",
+  },
+  {
+    id: 1,
+    question: "What is the purpose of the <head> tag in HTML?",
+    answer: `The <head> tag is used to include metadata and other information about the HTML document, such as the title of the page and links to external stylesheets.`,
+    category: "html",
+    description: "purpose of <head> tag",
+  },
+  {
+    id: 2,
+    question: "What is the role of the <a> tag in HTML?",
+    answer: `The <a> tag is used to create hyperlinks to other web pages, documents, or locations within the same page.`,
+    category: "html",
+    description: "<a> tag",
+  },
+  {
+    id: 3,
+    question: "What is the difference between the <ul> and <ol> tags in HTML?",
+    answer: `The <ul> tag is used to create an unordered list, while the <ol> tag is used to create an ordered list.`,
+    category: "html",
+    description: "<ul> & <ol> tags",
+  },
+  {
+    id: 4,
+    question: "What is the purpose of the alt attribute in the <img> tag?",
+    answer: `The alt attribute is used to provide alternative text for an image, which can be read by screen readers for users with visual impairments.`,
+    category: "html",
+    description: "<img> tag",
   },
 ];
