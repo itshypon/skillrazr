bfa882f925c127f3277d56a0da241fc6e51a215f
tukuna30 <tukuna.patro@gmail.com>
2023-05-07 20:08:12 +0530

    add story player with animations

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -9,28 +9,36 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
+import "../assets/css/Story.css";
+
+const sampleStory =
+  "Once upon a time there was a lion and tiger in a jungle. They were playing together for years. One day while the tiger was drinking water in the river a crocodile caught the tiger's leg. The tiger cried for help, The lion heard the tiger roar. He scared the crocodile and saved tigers life. The tiger and the lion were true friends. ";
 
 export default function Story() {
-  const [generatedStory, setGeneratedStory] = useState<string>("");
-  const [pickedCharacters, setPickedCharacters] = useState<string[]>([]);
+  const [generatedStory, setGeneratedStory] = useState<string>(
+    process.env.REACT_APP_ENV ? "" : sampleStory
+  );
+  const [selectedCharacters, setSelectedCharacters] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
   const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
   const [voice, setVoice] = React.useState<SpeechSynthesisVoice>();
+  const [playing, setPlaying] = useState(false);
+  const [openCurtains, setOpenCurtains] = useState(false);
 
   const handleChange = (event: SelectChangeEvent) => {
     setVoice(voices.find((v) => v.name === event.target.value));
   };
 
   const addRemoveCharacter = (character: string) => {
-    if (pickedCharacters.includes(character)) {
-      let array = [...pickedCharacters];
+    if (selectedCharacters.includes(character)) {
+      let array = [...selectedCharacters];
       array.splice(array.indexOf(character), 1);
-      setPickedCharacters(array);
+      setSelectedCharacters(array);
     } else {
-      if (pickedCharacters.length === 3) {
+      if (selectedCharacters.length === 3) {
         return;
       }
-      setPickedCharacters((previousValue) => [...previousValue, character]);
+      setSelectedCharacters((previousValue) => [...previousValue, character]);
     }
   };
 
@@ -47,7 +55,14 @@ export default function Story() {
     "🦍": "Gorilla",
     "🐉": "Dragon",
     "🐍": "Snake",
+    "🐪": "Camel",
+    "🐘": "Elephant",
+    "🦒": "Giraffe",
+    "🦜": "Parrot",
+    "🐝": "Bee",
+    "🦖": "Dinosaurs",
   };
+
   //function to generate character add buttons
   const renderCharacters = () => {
     let buttons = [];
@@ -63,7 +78,7 @@ export default function Story() {
         >
           <span
             className={`${
-              pickedCharacters.includes(character)
+              selectedCharacters.includes(character)
                 ? "border-2 bg-green-500"
                 : "border-0"
             }`}
@@ -72,6 +87,25 @@ export default function Story() {
           </span>
         </button>
       );
+    }
+    return buttons;
+  };
+
+  const renderSelectedCharacters = () => {
+    let buttons = [];
+    let index = 0;
+    for (const character of selectedCharacters) {
+      buttons.push(
+        <button
+          className="mr-2"
+          key={`${characters[character as keyof typeof characters]}-add-key`}
+        >
+          <span className={`actor actor${index} relative top-[5px]`}>
+            {character}
+          </span>
+        </button>
+      );
+      index++;
     }
     return buttons;
   };
@@ -94,7 +128,7 @@ export default function Story() {
   const generateStory = async () => {
     setGeneratedStory("");
     setLoading(true);
-    const actors = pickedCharacters.map((i) => characters[i]).join(",");
+    const actors = selectedCharacters.map((i) => characters[i]).join(",");
     console.log("selected chars", actors);
     try {
       let result = await storyGenerator(
@@ -113,15 +147,27 @@ export default function Story() {
     try {
       const message = new SpeechSynthesisUtterance(generatedStory);
       const synth = window.speechSynthesis;
-
+      setPlaying(true);
+      setTimeout(() => {
+        setOpenCurtains(true);
+      }, 100);
       if (voice) {
         message.voice = voice;
       }
       message.volume = 1;
       message.rate = 1;
 
-      synth.speak(message);
+      setTimeout(() => {
+        synth.speak(message);
+      }, 5000);
+
+      message.addEventListener("end", (event) => {
+        // setPlaying(false);
+        console.log(`Story finished in ${event.elapsedTime / 1000} seconds.`);
+        setOpenCurtains(false);
+      });
     } catch (e) {
+      setPlaying(false);
       console.log("error reading text", e);
     }
   };
@@ -129,24 +175,49 @@ export default function Story() {
   return (
     <div className="fixed w-full h-[100vh]" id="story">
       {/* div to add empty space (height) so below elements are not hidden by navbar */}
-      <main className="mt-[110px]">
-        <div className="text-4xl mb-6 border-b-2 border-gray-500 inline-block">
+      <main className="mt-[90px]">
+        <div className="text-4xl mb-6 border-b-2 border-gray-500 inline-block relative">
           Story Time!
+          <span className="absolute top-[16px] right-[-40px] text-5xl rotate-[15deg]">
+            🐒
+          </span>
         </div>
-        <p className="text-2xl">Pick 2 Characters!</p>
-        <div className="text-3xl my-6">{renderCharacters()}</div>
+        {playing ? (
+          <div className="border-[6px] border-gray-500">
+            <div
+              className={`text-3xl my-0 curtain ${
+                openCurtains ? " curtain-open" : " curtain-closed"
+              }`}
+            >
+              <div className="curtain-wrapper">
+                <div className="curtain-panel curtain-panel-left"></div>
+                <div className="curtain-content">
+                  {renderSelectedCharacters()}
+                </div>
+                <div className="curtain-panel curtain-panel-right"></div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <p className="text-xl">Pick 2 Actors</p>
+            <div className="text-3xl my-6">{renderCharacters()}</div>
+          </>
+        )}
         {loading ? (
           <CircularProgress />
         ) : (
           <>
-            <Button
-              className="w-[240px]"
-              variant="contained"
-              onClick={generateStory}
-              disabled={pickedCharacters.length === 0}
-            >
-              Generate
-            </Button>
+            {!playing && (
+              <Button
+                className="w-[240px]"
+                variant="contained"
+                onClick={generateStory}
+                disabled={selectedCharacters.length === 0}
+              >
+                Generate
+              </Button>
+            )}
 
             <div className="flex items-center justify-center mt-4">
               {generatedStory && voice ? (
